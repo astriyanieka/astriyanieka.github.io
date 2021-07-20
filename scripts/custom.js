b4693b8d5292594e6ad5dc1053110c14314a0b95
astriyanieka <astriyanieka21@gmail.com>
2021-07-20 15:24:24 +0700

    benerin design

--- scripts/custom.js
+++ scripts/custom.js
@@ -398,7 +398,7 @@ $(document).ready(function(){
             $('.single-slider').owlCarousel({loop:true, margin:20, nav:false, lazyLoad:true, items:1, autoplay: true, autoplayTimeout:4000});		
             $('.single-slider-boxed').owlCarousel({loop:true, stagePadding:30, margin:20, nav:false, lazyLoad:true, items:1, autoplay: true, autoplayTimeout:4000});		
             $('.cover-slider').owlCarousel({loop:true, margin:0, nav:false, lazyLoad:true, items:1, autoplay: true, autoplayTimeout:6000});		
-            $('.double-slider').owlCarousel({loop:true, margin:20, stagePadding:20, nav:false, lazyLoad:false, items:2, autoplay: false, autoplayTimeout:4000});		
+            $('.double-slider').owlCarousel({loop:true, margin:20, stagePadding:20, nav:false, lazyLoad:false, items:3, autoplay: true, autoplayTimeout:2000});		
             $('.task-slider').owlCarousel({loop:true, margin:20, nav:false, stagePadding:50, lazyLoad:true, items:2, autoplay: false, autoplayTimeout:4000});		
             $('.next-slide, .next-slide-arrow, .next-slide-text, .cover-next').on('click',function(){$(this).parent().find('.owl-carousel').trigger('next.owl.carousel');});		
             $('.prev-slide, .prev-slide-arrow, .prev-slide-text, .cover-prev').on('click',function(){$(this).parent().find('.owl-carousel').trigger('prev.owl.carousel');});		
@@ -1342,9 +1342,9 @@ $(document).ready(function(){
             var greetingTime = new Date().getHours();
             var greetingMessage;
             var greetingUser = $('.greeting-text').data('username')
-            var greetingMorning = ('Good morning');
-            var greetingAfternoon = ('Good afternoon');
-            var greetingEvening = ('Good evening');
+            var greetingMorning = ('Kami Mengundang');
+            var greetingAfternoon = ('Kami Mengundang');
+            var greetingEvening = ('Kami Mengundang');
 
             if (greetingTime >= 0 && greetingTime < 12) {greetingMessage = greetingMorning; 
             } else if (greetingTime >= 12 && greetingTime < 17) {greetingMessage = greetingAfternoon;
